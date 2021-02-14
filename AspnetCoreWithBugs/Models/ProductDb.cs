7b8ad853ebfdef2d607c37fb806f76ac8fc4f02e
Kramer Jensen <72529822+KramerJProg@users.noreply.github.com>
2021-02-14 14:15:27 -0800

    Created ProductDb class (#9)

---
+++ AspnetCoreWithBugs/Models/ProductDb.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspnetCoreWithBugs.Models
+{
+    public static class ProductDb
+    {
+    }
+}
